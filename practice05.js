07b3c91cccb8611d7e6542918d1763bd26646a40
IanYet <yt2015.97@gmail.com>
2017-04-28 18:31:17 +0800

    v-for

--- practice05.js
+++ practice05.js
@@ -38,3 +38,54 @@ var app03 = new Vue({
 var app04 = new Vue({
     el: '#app04'
 });
+
+Vue.component('my-com', {
+    props: ['item', 'index'],
+    template: '<p>{{index}}: {{item.text}}</p>'
+});
+
+var app05 = new Vue({
+    el: '#app05',
+    data: {
+        items:[{text: 'one'}, {text: 'two'}, {text: 'three'}]
+    }
+});
+
+Vue.component('todo-item', {
+    template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">X</button>\
+    </li>\
+  ',
+    props: ['title']
+})
+new Vue({
+    el: '#todo-list-example',
+    data: {
+        newTodoText: '',
+        todos: [
+            'Do the dishes',
+            'Take out the trash',
+            'Mow the lawn'
+        ]
+    },
+    methods: {
+        addNewTodo: function () {
+            this.todos.push(this.newTodoText)
+            this.newTodoText = ''
+        }
+    }
+})
+
+var app06 = new Vue({
+    el: '#app06',
+    data: {
+        todos:[
+            {value: 'taskOne', isComplete: true},
+            {value: 'taskTwo', isComplete: false},
+            {value: 'taskThree', isComplete: false},
+            {value: 'taskFour', isComplete: false}
+        ]
+    }
+});
