b7a2f3ed737c94b33dbbcdab3c9f52ed4d0652df
IanYet <yt2015.97@gmail.com>
2017-04-30 13:09:00 +0800

    v-model

---
+++ practice07.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Ian on 17/4/29.
+ */
+var app01 = new Vue({
+    el: '#app01',
+    data: {
+        message: ''
+    }
+});
+
+var app02 = new Vue({
+    el: '#app02',
+    data:{
+        checkedNames: []
+    }
+});
+
+var app03 = new Vue({
+    el: '#app03',
+    data:{
+        picked: ''
+    }
+});
