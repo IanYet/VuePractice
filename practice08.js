ddadc1fdc7927d3bd40bad0f011d75087f308cf7
IanYet <yt2015.97@gmail.com>
2017-05-04 09:52:53 +0800

    component

--- practice08.js
+++ practice08.js
@@ -11,5 +11,29 @@ var app00 = new Vue({
     el: '#app00',
     data: {
         inputMsg: 'hello'
+    },
+    computed: {
+        changeMsg: function () {
+            return this.inputMsg.trim().toUpperCase();
+        }
     }
+});
+
+Vue.component('com', {
+    template: '<div>' +
+    '<input v-model="message"/>' +
+    '<p>{{newMessage}}</p>' +
+    '</div>',
+    data: function () {
+        return {message: 'hello'}
+    },
+    computed:{
+        newMessage: function () {
+            return this.message.toUpperCase();
+        }
+    }
+});
+
+var  app01 = new Vue({
+    el: '#app01'
 });
