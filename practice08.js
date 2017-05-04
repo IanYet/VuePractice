c991932d7dead9de9a8c4dd02d6b877fac16386b
IanYet <yt2015.97@gmail.com>
2017-05-04 17:10:17 +0800

    component event

--- practice08.js
+++ practice08.js
@@ -2,19 +2,19 @@
  * Created by Ian on 17/5/3.
  */
 Vue.component('child', {
-    props: ["my-message"],
-    template:'<input :value="myMessage">',
+    props: {"my-message": Number},
+    template:'<input :value="myMessage">'
 
 });
 
 var app00 = new Vue({
     el: '#app00',
     data: {
-        inputMsg: 'hello'
+        inputMsg: 5
     },
     computed: {
         changeMsg: function () {
-            return this.inputMsg.trim().toUpperCase();
+            return this.inputMsg.valueOf()+1;
         }
     }
 });
@@ -36,4 +36,57 @@ Vue.component('com', {
 
 var  app01 = new Vue({
     el: '#app01'
+});
+
+Vue.component('button-counter', {
+    template: '<button v-on:click="increment">{{ counter }}</button>',
+    data: function () {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        increment: function () {
+            this.counter += 1;
+            this.$emit('increment');
+        }
+    }
+});
+var app02 = new Vue({
+    el: '#app02',
+    data: {
+        total: 0
+    },
+    methods: {
+        incrementTotal: function () {
+            this.total += 1;
+        }
+    }
+});
+
+
+Vue.component('currency-input', {
+    template: '\
+        <span>$\
+            <input\
+                ref="input"\
+                v-bind:value="value"\
+                v-on:input="updateValue($event.target.value)"/>\
+        </span>',
+    props: ['value'],
+    methods:{
+        updateValue: function (value) {
+            var formattedValue = value.trim().slice(0, value.indexOf('.')+3);
+
+            if(formattedValue !== value){
+                this.$refs.input.value = formattedValue;
+            }
+
+            this.$emit('input', Number(formattedValue));
+        }
+    }
+});
+
+var app03 = new Vue({
+    el: '#app03'
 });
